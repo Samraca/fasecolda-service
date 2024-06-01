5aaa16c7488f422ec58f8209bb767c8f7edfccbb
Samraca <Samraca@gmail.com>
2024-05-31 21:06:09 -0500

    Initial commit

---
+++ src/main/java/com/finalArqSoftware/fasecolda_service/FasecoldaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.finalArqSoftware.fasecolda_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasecoldaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasecoldaServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finalArqSoftware/fasecolda_service/FasecoldaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalArqSoftware.fasecolda_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasecoldaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
